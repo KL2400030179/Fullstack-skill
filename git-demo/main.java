0f0eec6edd209e1211bb281f42898b88d268b562
KL2400030179 <your email id>
2025-12-24 10:44:06 +0530

    bug-fix: adjust greeting message in main.java

--- git-demo/main.java
+++ git-demo/main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from git-demo project!"); 
         System.out.println("Feature update: new functionality added.");
+        System.out.println("Hello from git-demo project! (bug fixed)");
+
     }
 }
